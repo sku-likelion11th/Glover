384b7109154236805182270fa37a9aff6155380e
han-mg <alrb1277@gmail.com>
2023-09-28 19:13:41 +0900

    도장추가 완료

--- Glover_main/static/js/a_add.js
+++ Glover_main/static/js/a_add.js
@@ -59,28 +59,28 @@
 //     }
 // });
 
-// // form에서 이미지 업로드시 미리보기
-// function readURL(input, previewId) {
-//         if (input.files && input.files[0]) {
-//             var reader = new FileReader();
-//             reader.onload = function (e) {
-//                 document.getElementById(previewId).src = e.target.result;
-//             };
-//             reader.readAsDataURL(input.files[0]);
-//         } else {
-//             document.getElementById(previewId).src = "";
-//         }
-//     }
+// form에서 이미지 업로드시 미리보기
+function readURL(input, previewId) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById(previewId).src = e.target.result;
+            };
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            document.getElementById(previewId).src = "";
+        }
+    }
 
-// // 이미지 미리보기 상자를 클릭하면 파일 업로드 창 생성
-// const imgBoxes = document.querySelectorAll('.img-box');
+// 이미지 미리보기 상자를 클릭하면 파일 업로드 창 생성
+const imgBoxes = document.querySelectorAll('.img-box');
 
-// imgBoxes.forEach(function(imgBox) {
-//     imgBox.addEventListener('click', function() {
-//         const fileInput = imgBox.closest('.file-box').querySelector('input[type="file"]');
-//         fileInput.click();
-//     });
-// });
+imgBoxes.forEach(function(imgBox) {
+    imgBox.addEventListener('click', function() {
+        const fileInput = imgBox.closest('.file-box').querySelector('input[type="file"]');
+        fileInput.click();
+    });
+});
 
 
 
